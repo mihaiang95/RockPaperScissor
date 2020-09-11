f9658783c2067ada85317837ea3d94da48666563
Mihai <anghelmihaialexandru@gmail.com>
2020-09-11 22:25:55 +0300

    Initial commit

---
+++ Game/Game.cs
@@ -0,0 +1,33 @@
+﻿using RockPaperScissor.Players;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissor.Game
+{
+    public class Game
+    {
+        IPlayer player1;
+        IPlayer player2;
+
+        int numberOfGamesPlayed;
+        List<Round> roundsHistory;
+
+        GameStates state;
+        int winner;
+
+        public void Startgame()
+        {
+
+        }
+    }
+
+    public enum GameStates 
+    {
+        Started = 0,
+        InProgress = 1,
+        Finished = 2
+    }
+
+    
+}
